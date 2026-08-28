86901d65af17142cf200dadce8fb48be43fc0464
Nomonjon-nate <begmatovnomonjon6@gmail.com>
2026-08-28 20:24:13 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
 // 1: Kirish codes
 app.use(express.static("public"));
@@ -26,6 +36,10 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, resizeBy) => {
     console.log(req);
     resizeBy.json({ test: "success" });
+});
+
+app.get('/author', (req, resizeBy) => {
+    resizeBy.render("author", {user: user });
 });
 
 app.get("/", function(req, resizeBy) {
