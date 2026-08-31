08d75ba80a05967370c0a5f035443fe8550e6312
Nomonjon-nate <begmatovnomonjon6@gmail.com>
2026-09-01 01:29:17 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -43,11 +43,12 @@ app.get('/author', (req, resizeBy) => {
 });
 
 app.get("/", function(req, resizeBy) {
-    resizeBy.render("harid");
+    resizeBy.render("reja");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
-    console.log(`The server is running successfully on port: ${PORT}`);
+    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+    );
 });
